5259883c2ad3c22671241677018c427077d2ef7c
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-10-03 07:48:51 +0700

    Jobsheet 6

---
+++ Pemilihan2Percobaan1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan1 {
+    public static void main(String[] args) {
+        Scanner input27 = new Scanner(System.in);
+        float tahun;
+
+        System.out.print("masukan tahun :");
+        tahun = input27.nextFloat();
+
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) != 0) 
+                System.out.print("Tahun Kabisat");
+        }else 
+            System.out.print("Bukan Tahun Kabisat");
+    }
+    
+}
