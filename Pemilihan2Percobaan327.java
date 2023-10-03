04189571dfc7da016c6d5ea71f76b3ffe5d3fb20
TomiMartinoAffandi <tomimartino10@gmail.com>
2023-10-03 12:32:46 +0700

    project akhir

--- Pemilihan2Percobaan327.java
+++ Pemilihan2Percobaan327.java
@@ -1,6 +1,42 @@
-import java.util.Scanner;
-public class Pemilihan2Percobaan327 {
-    public static void main(String[] args) {
-        
+    import java.util.Scanner;
+    public class Pemilihan2Percobaan327 {
+        public static void main(String[] args) {
+            Scanner input27 = new Scanner(System.in);
+
+            String kategori;
+            int penghasilan, gajiBersih;
+            double pajak=0;
+
+            System.out.println("masukan kategori");
+            kategori = input27.nextLine();
+            System.out.println("masukan besarnya penghasilan");
+            penghasilan = input27.nextInt();
+
+            if (kategori.equals("pekerja")) {
+                if (penghasilan <= 2000000) {
+                    pajak = 0.1;
+                }
+                else if (penghasilan <= 3000000) {
+                    pajak = 0.15;
+                }
+                else{
+                    pajak = 0.2;
+                }
+                gajiBersih = (int) (penghasilan - (pajak * penghasilan));
+                System.out.println("penghasilan bersih" + gajiBersih);
+            }else if (kategori.equals("pebisnis")) {
+                if (penghasilan <= 2500000) {
+                    pajak = 0.15;
+                } else if (penghasilan <= 3500000) {
+                    pajak = 0.2;
+                } else{
+                    pajak = 0.25;
+                }
+                gajiBersih = (int) (penghasilan - (pajak *penghasilan));
+                System.out.println("penghasilan bersih " + gajiBersih);
+                
+            }else{
+            System.out.println("masukan kategori salah");
+            }
+        }
     }
-}
